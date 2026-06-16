9eac02ceaecfa687229f4d6e5c5523c7ee3dd0b8
Mirudula Devi <smiruduladevi173@gmail.com>
2026-06-16 14:35:02 +0530

    LoginController - library issue

--- src/main/java/srms_web/controller/LoginController.java
+++ src/main/java/srms_web/controller/LoginController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.ui.Model;
+
 import srms_web.auth.Authentication;
 
 @Controller
