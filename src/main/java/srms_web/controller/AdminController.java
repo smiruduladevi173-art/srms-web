5b2bd707f4e2e638da1cb9a7c56c2a2aef7a5590
Mirudula Devi <smiruduladevi173@gmail.com>
2026-06-18 14:49:51 +0530

    Merging Admin-Module branch and main branch(BM)

--- src/main/java/srms_web/controller/AdminController.java
+++ src/main/java/srms_web/controller/AdminController.java
@@ -1,9 +1,5 @@
 package srms_web.controller;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import srms_web.model.AnalyticsSummary;
 import srms_web.model.Student;
 
 import srms_web.admin.AdminService;
-import srms_web.database.DBConnection;
-
-import java.sql.Connection;
 
 @Controller
 public class AdminController {
